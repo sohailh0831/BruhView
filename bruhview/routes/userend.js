8461da5355e5902cea795c36a5de20d1923f2c25
sohailh0831 <32226619+sohailh0831@users.noreply.github.com>
2019-04-16 23:45:30 -0400

    Initial Commit
    Includes the CSS template that will be used for this site, and includes the newly created node server and userend.js file.

---
+++ bruhview/routes/userend.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const _ = require('lodash');
+const session = require('express-session');
+const expressValidator = require('express-validator');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const bcrypt= require('bcryptjs');
+const path = require('path');
+const flash = require('connect-flash');
+const exphbs = require('express-handlebars');
+const uuidv4 = require('uuid/v4');
+var passport = require("passport");
+var request = require("request");
+const mysql = require('mysql');
+const moment = require('moment');
+let dbInfo = {
+  host: "localhost",
+  user: "root",
+  password: "cs252project!",
+  database : 'BruhView'
+};
+
+console.log("HH");
+router.get('/dashboard', (req, res) => {
+    return res.render('platform/dashboard.hbs', {
+    error: req.flash('error'),
+    success: req.flash('success'),
+  });
+    console.log("here");
+});
+
+
+module.exports = router;
