f76f74c962373b0c3f4ba27b551740d949991ab2
Haris Sohail <sohailh@purdue.edu>
2019-04-19 11:13:35 -0400

    update userend.js

--- bruhview/routes/userend.js
+++ bruhview/routes/userend.js
@@ -14,12 +14,18 @@ var passport = require("passport");
 var request = require("request");
 const mysql = require('mysql');
 const moment = require('moment');
+
 let dbInfo = {
   host: "localhost",
   user: "root",
   password: "cs252project!",
   database : 'BruhView'
 };
+
+
+app.use(session({ secret: 'somekey' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const LocalStrategy = require('passport-local').Strategy;
 const AuthenticationFunctions = require('../Authentication.js');
@@ -44,35 +50,6 @@ router.post('/login', AuthenticationFunctions.ensureNotAuthenticated, passport.a
   res.redirect('/dashboard');
 });
 
-/*
-router.post('/login',(req,res) => {
-      let username = req.body.username;
-      let password = req.body.password;
-      req.checkBody('username', 'Username field is required.').notEmpty();
-      req.checkBody('password', 'Password field is required.').notEmpty();
-      let formErrors = req.validationErrors();
-        if (formErrors) {
-            req.flash('error', formErrors[0].msg);
-            return res.redirect('/login');
-        }
-        let con = mysql.createConnection(dbInfo);
-        con.query(`SELECT * FROM users WHERE username=${mysql.escape(req.body.username)} AND password=${mysql.escape(req.body.password)};`, (error, results, fields) => {
-          if (error) {
-            console.log(error.stack);
-            con.end();
-            return res.send();
-          }
-            if(results.length == 0){
-              con.end();
-              req.flash('success', 'Username or Password are incorrect');
-              return res.redirect('/login');
-            } // user not found
-            con.end();
-            req.flash('success', 'Successfully Logged In');
-            return res.redirect('/dashboard');
-        });
-});
-*/
 passport.use(new LocalStrategy({passReqToCallback: true,},
 	function (req, username, password, done) {
       let con = mysql.createConnection(dbInfo);
@@ -87,7 +64,6 @@ passport.use(new LocalStrategy({passReqToCallback: true,},
           return done(null, false, req.flash('error', 'Username or Password is incorrect.'));
         } else {
             if (bcrypt.compareSync(password, results[0].password)) {
-              console.log(`${username} successfully logged in.`);
               let user = {
                   identifier: results[0].id,
                   username: results[0].username,
