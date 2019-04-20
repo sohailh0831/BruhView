01529a194df38bd436f5a144915b0659b28828f3
Haris Sohail <sohailh@purdue.edu>
2019-04-19 20:36:47 -0400

    update userend

--- bruhview/routes/userend.js
+++ bruhview/routes/userend.js
@@ -16,10 +16,25 @@ const mysql = require('mysql');
 const moment = require('moment');
 
 let dbInfo = {
-  host: "localhost",
+
+  connectionLimit : 100,
+  host: "134.209.4.10",
+  port: "80",
   user: "root",
   password: "cs252project!",
   database : 'BruhView'
+
+/*
+  connectionLimit: 100,
+   host:'134.209.4.10',
+   user:'root',
+   password:'cs252project!',
+   database:'BruhView',
+   port: 80,
+   debug: false,
+   multipleStatements: true
+
+   */
 };
 
 const LocalStrategy = require('passport-local').Strategy;
@@ -84,6 +99,14 @@ passport.use(new LocalStrategy({passReqToCallback: true,},
       });
 
 }));
+
+passport.serializeUser(function (uuid, done) {
+	done(null, uuid);
+});
+
+passport.deserializeUser(function (uuid, done) {
+  done(null, uuid);
+});
 
 
 
@@ -246,24 +269,10 @@ router.post('/registeradmin',AuthenticationFunctions.ensureAuthenticated,(req,re
 });
 
 
-
-
-
-
-
-
 router.get('/logout', AuthenticationFunctions.ensureAuthenticated, (req, res) => {
   req.logout();
   req.session.destroy();
   return res.redirect('/login');
-});
-
-passport.serializeUser(function (uuid, done) {
-	done(null, uuid);
-});
-
-passport.deserializeUser(function (uuid, done) {
-  done(null, uuid);
 });
 
 
